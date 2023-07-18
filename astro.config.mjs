61be9d1eb3bc92d4b1ac429baeebddfda59e8f4b
vorlie <vorlie0961@gmail.com>
2023-07-18 06:25:37 +0200

    website moved and redo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    routes: {
+        '/*': './src/pages/404.astro', 
+      },
+      experimental: {
+        redirects: true
+      },
+      redirects: {
+        '/discord': 'https://discord.gg/NMxKTbQcDZ'
+      }
+});
