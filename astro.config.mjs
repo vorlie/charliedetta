aa6451c3a3dbd8601ae6f1e64078dc4df0da850b
vorlie <vorlie0961@gmail.com>
2023-07-19 15:15:40 +0200

    changed the redirect link

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
         '/r/youtube':'https://youtube.com/@vve1_',
         '/r/ugg':'https://u.gg/lol/profile/eun1/the%20mighty%20dio/overview',
         '/oneevv/invite':'https://discord.com/api/oauth2/authorize?client_id=1060061912710258699&permissions=10311612919030&scope=bot%20applications.commands',
-        '/r/github/sourcecode':'https://github.com/vorlie/1evv'
+        '/oneevv/sourcecode':'https://github.com/vorlie/1evv'
       }
 });
