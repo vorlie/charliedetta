91d8c88b8af2e5b5973c33e95b908f69723f3d58
vorlie <vorlie0961@gmail.com>
2023-07-19 15:11:51 +0200

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     routes: {
         '/*': './src/pages/404.astro',
-        '/search': './src/pages/search.astro'
+        '/search': './src/pages/search.astro',
+        '/oneevv':'./src/pages/oneevv.astro'
       },
       experimental: {
         redirects: true
@@ -13,6 +14,8 @@ export default defineConfig({
         '/discord': 'https://discord.gg/NMxKTbQcDZ',
         '/r/github': 'https://github.com/vorlie',
         '/r/youtube':'https://youtube.com/@vve1_',
-        '/r/ugg':'https://u.gg/lol/profile/eun1/the%20mighty%20dio/overview'
+        '/r/ugg':'https://u.gg/lol/profile/eun1/the%20mighty%20dio/overview',
+        '/oneevv/invite':'https://discord.com/api/oauth2/authorize?client_id=1060061912710258699&permissions=10311612919030&scope=bot%20applications.commands',
+        '/r/github/sourcecode':'https://github.com/vorlie/1evv'
       }
 });
