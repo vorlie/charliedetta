2180e0c31e1d999dca2fceacf114b429796ff308
vorlie <vorlie0961@gmail.com>
2024-06-07 03:00:03 +0200

    Refactor: remove redirects and add links directly to a tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,4 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
-    redirects: {
-        '/discord': 'https://discord.gg/ujYCjUwu6U',
-        '/r/github': 'https://github.com/vorlie',
-        '/r/youtube':'https://youtube.com/@vve1_',
-        '/r/faceit':'https://www.faceit.com/en/players/vorlie',
-        '/r/ugg':'https://u.gg/lol/profile/eun1/yae%20miko-noway/overview',
-        '/miko/invite':'https://discord.com/api/oauth2/authorize?client_id=1060061912710258699&permissions=10309468548118&scope=bot+applications.commands'
-      }
 });
